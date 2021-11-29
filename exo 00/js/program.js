f517711441190955379e0f4f4bbf4fc2406326c2
matvki <m.vecchi@it-students.fr>
2021-11-29 10:37:09 +0100

    end exo 00

---
+++ exo 00/js/program.js
@@ -0,0 +1,41 @@
+let array
+let targetSum
+let test
+let tamp = null
+let tampNumber
+let alreadyPrint = 0
+
+document.querySelector('#submit').addEventListener('click', () => {
+    array = document.querySelector('#numbers').value.split(',')
+    targetSum = document.querySelector('#target').value
+    twoNumberSum(array, targetSum)
+})
+
+function twoNumberSum(array, targetSum){
+    targetSum = parseInt(targetSum)
+    array.forEach(function (number, i) {
+        array.forEach(function (num, j) {
+            alreadyPrint = 0
+            if (i !== j) {
+                test = parseInt(number, 10) + parseInt(num, 10)
+                if (test === targetSum) {
+                    if (tamp !== null) {
+                        tamp.forEach(function (value) {
+                            tampNumber = num + ' + ' + number + ' = ' + targetSum
+                            if (value === tampNumber) {
+                                alreadyPrint = 1
+                            }
+                        })
+                    }
+                    if (alreadyPrint !== 1) {
+                        console.log(number + ' + ' + num + ' = ' + targetSum)
+                        tamp = [number + ' + ' + num + ' = ' + targetSum]
+                    }
+                }
+            }
+        })
+    })
+}
+
+// Merci de ne pas effacer la ligne en dessous.
+// exports.twoNumberSum =  twoNumberSum;
