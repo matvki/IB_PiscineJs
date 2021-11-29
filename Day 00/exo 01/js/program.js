767ccd07753985aeb9f794a72f7f45f4efa25b63
matvki <m.vecchi@it-students.fr>
2021-11-29 14:21:51 +0100

    patch exo 1

--- Day 00/exo 01/js/program.js
+++ Day 00/exo 01/js/program.js
@@ -1,17 +1,20 @@
 let string
 let tamp
+let result
 
 document.querySelector('#submit').addEventListener('click', () => {
     string = document.querySelector('#string').value
-    isPalindrome(string)
+    result = isPalindrome(string)
+    console.log(result);
 })
 
 function isPalindrome(string){
     tamp = string.split('').reverse().join('')
     if (tamp === string) {
         document.write('<p>'+string+' est un palindrome</p>')
-        console.log('Vrai')
+        return true
     }
+    return false
 }
 
 // Merci de ne pas effacer la ligne en dessous.
