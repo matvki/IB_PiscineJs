e3ddc8fe5f54fba33fb6f57e38b311f2d666e085
matvki <m.vecchi@it-students.fr>
2021-11-29 15:30:43 +0100

    patch rush 00

--- Day 00/rush 00/js/program.js
+++ Day 00/rush 00/js/program.js
@@ -5,7 +5,8 @@ function sun(buildings, direction){
     let length = buildings.length
     let result = []
     let tamp = null
-    if (direction === 'Ouest') {
+
+    if (direction.toLowerCase() === 'ouest') {
         result.push(0)
         for (let i=1;i<length;i++) {
             if ( tamp !== null) {
@@ -19,7 +20,7 @@ function sun(buildings, direction){
                 tamp = i-1
             }
         }
-    } else if (direction === 'Est') {
+    } else if (direction.toLowerCase() === 'est') {
         result.push(length-1)
         for (let i=length-2;i>=0;i--) {
             if ( tamp !== null) {
